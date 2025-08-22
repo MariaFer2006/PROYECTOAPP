84e68a52fb91a55b13272c35eb4091e0671251d6
Maria Fernanda Herazo <mafeherazoescobar@gmail.com>
2025-08-22 18:43:51 -0500

    Primer commit: backend y frontend

---
+++ copia/backend/src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+
+const app = express();
+const PORT = 4000;
+
+// Conectar a BD
+connectDB();
+
+// Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+// Rutas
+const clienteRoutes = require('./routes/clienteRoutes');
+app.use('/api/clientes', clienteRoutes);
+
+// Servidor
+app.listen(PORT, () => console.log(`Servidor backend corriendo en http://localhost:${PORT}`));
